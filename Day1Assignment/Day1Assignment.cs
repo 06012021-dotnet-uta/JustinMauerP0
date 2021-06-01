6505855608db9e96c406f2d30fc1bfd8b69196e6
jdmauer <justin.mauer@revature.net>
2021-06-01 18:27:58 -0400

    Updated Day1Assignment

---
+++ Day1Assignment/Day1Assignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day1Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enter Name
+            string name;
+            Console.Write("Enter Name: ");
+            name = Console.ReadLine();
+            
+            Console.Write("Enter age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Name: " + name + "\nAge: " + age);
+
+        }
+    }
+}
